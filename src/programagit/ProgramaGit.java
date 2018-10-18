b8f1433c19443343978e31225df81a30af34fe09
Washington Ribeiro <washingtonraraujo9@gmail.com>
2018-10-18 14:03:47 -0300

    Mensagem boa tarde

--- src/programagit/ProgramaGit.java
+++ src/programagit/ProgramaGit.java
@@ -15,7 +15,8 @@ public class ProgramaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bom dia");
+        System.out.println("Boa tarde");
     }
     
 }
